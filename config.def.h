7732783ca79ce2ce419d6e986a9e5db4120bf991
askfiy <c2323182108@gmail.com>
2022-04-20 17:46:15 +0800

    disable default theme, apply catppuccin themes

--- config.def.h
+++ config.def.h
@@ -15,15 +15,17 @@ static const int topbar             = 1;     /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "agave Nerd Font:size=14", "JoyPixels:size=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "agave Nerd Font:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char normal_fg[]       = "#D9E0EE";
+static const char normal_bg[]       = "#1E1E2E";
+static const char normal_bd[]       = "#575268";
+static const char focus_fg[]        = "#D9E0EE";
+static const char focus_bg[]        = "#575268";
+static const char focus_bd[]        = "#B5E8E0";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normal_fg, normal_bg, normal_bd },
+	[SchemeSel]  = { focus_fg,  focus_bg,  focus_bd  },
 };
 
 /* tagging */
@@ -38,7 +40,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                     instance    title       tags mask     isfloating   monitor */
-	{"Wine",                      NULL,      NULL,       1 << 7,       1,              -1}
+	{"Wine",                      NULL,      NULL,       1 << 7,       1,              -1},
 	{"picgo",                     NULL,      NULL,       1 << 8,       1,              -1},
 	{"Clash for Windows",         NULL,      NULL,       1 << 8,       1,              -1},
 };
