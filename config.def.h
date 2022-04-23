88fefb56170e6c8b6cee4cd253d94efa56b1a64e
askfiy <c2323182108@gmail.com>
2022-04-23 17:52:05 +0800

    Modify taskbar icon size

--- config.def.h
+++ config.def.h
@@ -13,7 +13,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 0;     /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "agave Nerd Font:size=14", "JoyPixels:size=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "agave Nerd Font:size=14", "JoyPixels:size=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "agave Nerd Font:size=14";
 static const char normal_fg[]       = "#D9E0EE";
 static const char normal_bg[]       = "#1E1E2E";
