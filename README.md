6ace66271cd57891c8f3ef9e1f84f68675619d24
askfiy <c2323182108@gmail.com>
2024-04-04 21:31:08 +0800

    chore(config.h): Modify the default configuration to make it more reasonable

---
+++ README.md
@@ -0,0 +1,78 @@
+# README
+
+## Introduction
+
+A `dwm` dynameic window manager built by yourself.
+
+## Depends
+
+Install all Depends:
+
+```
+$ yay -S st
+$ yay -S picom
+$ yay -S dmenu
+$ yay -S nerd-fonts-go
+```
+
+## Disposition
+
+Edit the `$HOME/.xinitrc` file:
+
+```sh
+exec dwm
+```
+Add the startup script file, `$HOME/.local/share/dwm/autostart.sh`, this is my autostart.sh example:
+
+```sh
+#!/bin/bash
+
+# disable touchpad for 0.5s while typing
+syndaemon -R -i 0.5 &
+
+# auto screen
+if xrandr | grep -wq "DP1 connected"; then
+	xrandr --output DP1 --mode 2560x1440 --rate 165 --primary --output eDP1 --off
+fi
+
+function if_not_exists_else_running() {
+	STATUS=$(ps -C $1 --no-header | wc -l)
+	echo $1 $STATUS
+	if [ $STATUS -eq 0 ]; then
+		$1 &
+	fi
+}
+
+if_not_exists_else_running picom
+if_not_exists_else_running clash
+if_not_exists_else_running fcitx5
+if_not_exists_else_running slstatus
+
+feh --bg-scale $HOME/.local/share/dwm/wallpaper/jinx.jpg
+```
+
+## Some keys
+
+The buttons below are adjusted:
+
+```
+modkey                  : Super_L
+
+modkey + o              : open float terminal (st)
+modkey + q              : kill client
+modkey + +              : increase the spacing
+modkey + -              : decrease the spacing
+modkey + 0              : resize the spacing
+modkey + r              : restart dwm
+
+modkey + return         : open terminal (st)
+modkey + shift + 0      : show all tag content
+modkey + shift + return : toggles the relative position of the focal point
+
+shift + f1              : toggle the audio switch（pipewire-pulse）
+shift + f2              : increase the volume（pipewire-pulse）
+shift + f3              : decrease the volume（pipewire-pulse）
+
+shift + f5              : decrease the backlight 
+shift + f6              : increase the backlight 
+```
--- config.h
+++ config.h
@@ -1,3 +1,4 @@
+
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -12,17 +13,20 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "monospace:size=14", "JoyPixels:size=14:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "GoMono Nerd Font:style=Book:pixelsize=18:antialias=true:autohint=true", "JoyPixels:size=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "GoMono Nerd Font:style=Book:pixelsize=18:antialias=true:autohint=true";
+
+static const char no_active_fg[]       = "#bbbbbb";
+static const char no_active_bg[]       = "#222222";
+static const char no_active_bd[]       = "#444444";
+static const char in_active_fg[]       = "#eeeeee";
+static const char in_active_bg[]       = "#005577";
+static const char in_active_bd[]       = "#96cdf8";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { no_active_fg, no_active_bg, no_active_bd },
+	[SchemeSel]  = { in_active_fg, in_active_bg, in_active_bd },
 };
 
 /* tagging */
@@ -33,9 +37,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                  instance     title       tags mask     isfloating   monitor */
+	{ "Gimp",                   NULL,       NULL,         0,            1,           -1 },
+	{ "Firefox",                NULL,       NULL,         1 << 8,       0,           -1 },
+    { "Clash for Windows",      NULL,       NULL,         1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
@@ -64,10 +69,26 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb",  "#222222", "-nf",  "#bbbbbb", "-sb", "#005577", "-sf", "#eeeeee", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "180x40", NULL };
+
+// voice control
+#define XF86_F1 0xffbe
+#define XF86_F2 0xffbf
+#define XF86_F3 0xffc0
+
+static const char *incr_volume[] = { "amixer", "sset", "Master", "5%+", "unmute", NULL };
+static const char *decr_volume[] = { "amixer", "sset", "Master", "5%-", "unmute", NULL };
+static const char *toggle_volume[] = { "amixer", "sset", "Master", "toggle", NULL };
+
+// screen backlight
+#define XF86_F5 0xffc2
+#define XF86_F6 0xffc3
+
+static const char *incr_backlight[] = { "xbacklight", "-inc", "10", NULL };
+static const char *decr_backlight[] = { "xbacklight", "-dec", "10", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -97,7 +118,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY,                       XK_0,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_0,  setgaps,            {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -107,6 +128,11 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ShiftMask,                     XF86_F1,    spawn,          {.v = toggle_volume } },
+	{ShiftMask,                     XF86_F2,    spawn,          {.v = decr_volume } },
+	{ShiftMask,                     XF86_F3,    spawn,          {.v = incr_volume } },
+	{ShiftMask,                     XF86_F5,    spawn,          {.v = decr_backlight } },
+	{ShiftMask,                     XF86_F6,    spawn,          {.v = incr_backlight } },
 	{ MODKEY,                       XK_r,      quit,           {1} }, 
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
